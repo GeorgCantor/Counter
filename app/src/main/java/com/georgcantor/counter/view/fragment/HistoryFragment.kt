26d8dd15c3000b43916d2918abccb769047a378e
GeorgCantor <cupsman1986@gmail.com>
2020-01-03 21:21:11 +0300

    changed buttons color

--- app/src/main/java/com/georgcantor/counter/view/fragment/HistoryFragment.kt
+++ app/src/main/java/com/georgcantor/counter/view/fragment/HistoryFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.georgcantor.counter.R
@@ -38,8 +37,6 @@ class HistoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val navOption = NavOptions.Builder().setLaunchSingleTop(true).build()
-
         daysRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         daysRecyclerView.setHasFixedSize(true)
 
@@ -51,7 +48,7 @@ class HistoryFragment : Fragment() {
             }, {
                 requireContext().showDialog(getString(R.string.remove)) {
                     viewModel.removeDay(it.id)
-                    Navigation.findNavController(view).navigate(R.id.historyFragment, null, navOption)
+                    requireActivity().recreate()
                 }
             })
         })
